f1743f28a5cbc1b2bea7f675e8641724e5b87ca2
ValentineGhesq <ValentineGhesq@users.noreply.github.com>
2020-11-17 18:41:44 +0100

    correction bug

--- traitement/recherche.php
+++ traitement/recherche.php
@@ -4,9 +4,11 @@
     $query= $pdo->prepare($sql);
     $query->execute(); 
     while($line=$query->fetch()){
-        if((strstr($line['login'],$_POST['recherche'])==TRUE)){
-            echo"<a href='index.php?action=mur&id=".$line['id']."'>".$line['login']."</a>";
-        }
+        if(isset($_POST['recherche'])){
+            if((strstr($line['login'],$_POST['recherche'])==TRUE)){
+                echo"<a href='index.php?action=mur&id=".$line['id']."'>".$line['login']."</a>";
+            }
+    }
     
     }
 
