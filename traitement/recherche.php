8198dd8adb655b7f7db5a51ad2e75864d51edd02
ValentineGhesq <ValentineGhesq@users.noreply.github.com>
2020-11-30 12:06:04 +0100

    modif bug

--- traitement/recherche.php
+++ traitement/recherche.php
@@ -3,7 +3,7 @@
     $sql = "SELECT * FROM user WHERE id NOT IN ( SELECT user.id FROM user INNER JOIN lien ON idUtilisateur1=user.id AND etat='ami' AND idUTilisateur2=? UNION SELECT user.id FROM user INNER JOIN lien ON idUtilisateur2=user.id AND etat='ami' AND idUTilisateur1=?)";
    
     $query= $pdo->prepare($sql);
-    $query->execute(array($_SESSION['id'], $_SESSION['id'], $_SESSION['id'])); 
+    $query->execute(array( $_SESSION['id'], $_SESSION['id'])); 
     while($line=$query->fetch()){
         if(isset($_POST['recherche'])){
             if((strstr($line['login'],$_POST['recherche'])==TRUE)){
